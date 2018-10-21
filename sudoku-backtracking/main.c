441f0a4e98db9270ec06475970dbac6e9a9e5cd7
Luan H Alves <luan.henrique20@gmail.com>
2018-10-21 12:14:24 -0300

    Initial Commit

---
+++ sudoku-backtracking/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sudoku-backtracking
+//
+//  Created by Luan H Alves on 21/10/18.
+//  Copyright © 2018 Luan H Alves. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
